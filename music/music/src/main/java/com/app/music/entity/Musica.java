bd9304a59333ae12f5872dfb927800300e9ad3c2
Leandro Costa Magalhães Silva <leandrodevbackend@gmail.com>
2026-01-15 13:41:24 -0400

    update

--- music/music/src/main/java/com/app/music/entity/Musica.java
+++ music/music/src/main/java/com/app/music/entity/Musica.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.ArrayList; 
+
+
 @Entity
 @Table(name = "musicas")
 @Data
